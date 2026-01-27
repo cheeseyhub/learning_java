3fa0ea612bb5eeadf3db2784a5e53f30c7fe128e
CheeseyHub <muhammedibrahim613@gmail.com>
2026-01-27 05:58:07 +0500

    Practicing the book

---
+++ basics/Example2.java
@@ -0,0 +1,15 @@
+class Example2 {
+    public static void main(String args[])
+    {
+        int num;
+        num = 100;
+
+        System.out.println("This is a num :" + num);;
+
+        num = num * 2;
+
+        System.out.print("The vale of num * 2 is ");
+        System.out.print(num);
+
+    }
+}
