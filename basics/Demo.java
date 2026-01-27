cd7f3369c2ff8ff10988c176c75657711977195f
CheeseyHub <muhammedibrahim613@gmail.com>
2026-01-27 18:45:27 +0500

    learned stack in java

---
+++ basics/Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+   public static void main(String args[]) 
+   {
+    Stack myStack = new Stack();
+    myStack.push(99);
+    myStack.push(100);
+    myStack.push(12389);
+    myStack.push(288);
+
+    System.out.println(myStack.pop());
+    System.out.println(myStack.peek());
+
+
+   }
+    
+}
---
+++ basics/Stack.java
@@ -0,0 +1,34 @@
+public class Stack {
+    int[] stack = new int[10];
+    int tos;
+
+    Stack() {
+        tos = -1;
+    }
+
+    void push(int item) {
+        if(tos == 9)
+        {
+            System.out.println("Stack is full");
+        }
+        else {
+            stack[++tos] = item;
+        }
+    }
+
+    int pop() {
+        if (tos < 0)
+        {
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        else
+        {
+            return stack[tos--];
+        }
+    }
+    int peek() {
+        return stack[tos];
+    }
+}
+
