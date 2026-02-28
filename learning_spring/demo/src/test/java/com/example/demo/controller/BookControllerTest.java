0648a4380c1698217f476869d0f8b632a824addc
CheeseyHub <muhammedibrahim613@gmail.com>
2026-02-28 08:43:50 +0500

    learned post testing in spring boot

--- learning_spring/demo/src/test/java/com/example/demo/controller/BookControllerTest.java
+++ learning_spring/demo/src/test/java/com/example/demo/controller/BookControllerTest.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.model.Book;
+
+import tools.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -12,24 +19,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class BookControllerTest {
     @Autowired
-    private MockMvc mockMvc;
-
-
+    private MockMvc  mockMvc;
 
     @Test
-    void testDisplayBook() throws Exception{
-        mockMvc.perform(get("/books")).andExpect(status().isOk()).andExpect(content().string("List of Books."));
+    public void testGetBooks() throws Exception{
+        mockMvc.perform(get("/books"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("List of Books."));
+
         
-
     }
-
     @Test
-    void testGetBooks() {
+    public void testDisplayBook() throws Exception{
+        Book testBook = new Book();
+        testBook.setTitle("Levy");
+        mockMvc.perform(post("/books")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(testBook)))
+        .andExpect(status().isOk())
+        .andExpect(content().string("The title of the book is Levy"));
 
     }
 
-    @Test
-    void testUpdateBook() {
-
-    }
 }
